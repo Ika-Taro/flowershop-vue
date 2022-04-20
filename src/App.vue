e9e9bd75605b5a0aae59b293725cd38d445c93c5
Ika-Taro <black.jkh@gmail.com>
2022-04-20 11:04:34 +0900

    footer作成

--- src/App.vue
+++ src/App.vue
@@ -2,18 +2,21 @@
   <v-app>
     <v-main>
       <HelloWorld/>
+      <AppFooter/>
     </v-main>
   </v-app>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import AppFooter from './components/AppFooter.vue'
 
 export default {
   name: 'App',
 
   components: {
     HelloWorld,
+    AppFooter
   },
 
   data: () => ({
